1b9253e4b5faefe0ced03673993c93f043676943
hilshire <hillshire.triela@gmail.com>
2024-03-24 20:33:31 +0800

    build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: [
+            'typeorm',
+            'bcryptjs',
+            'jsonwebtoken',
+            'mysql'
+        ]
+    }
+};
 
 export default nextConfig;
